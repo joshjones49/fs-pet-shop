a854614b40e9510584b66054dc734ec446a670ef
joshjones49 <joshjones200285@gmail.com>
2023-11-02 18:55:42 -0500

    updated and refactored code

--- 3-express/express.js
+++ 3-express/express.js
@@ -1,26 +1,32 @@
-//require dependencies and get paths---------
-const fs = require('fs')
-const express = require('express')
+const fs = require('fs');
+const express = require('express');
 const app = express();
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
 const port = 8000;
-//---------------------------
 
-app.get('/pets', (req, res) => {
-    fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-        if(err) {
-            res.statusCode = 500
-            res.setHeader('Content-Type', 'text/plain')
-            res.send('Server Error')
-        }
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain');
-        res.send(petsJSON);
-    })
-     
-})
-//------listener------------
+app.get('/pets/:index', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      res.status = 500
+      res.send('Server Error');
+      return;
+    }
+
+    const pets = JSON.parse(petsJSON);
+    const index = parseInt(req.params.index);
+
+    if (typeof index !== 'number' || index < 0 || index >= pets.length) {
+      res.status = 404
+      res.send('Not Found');
+      return;
+    }
+
+    res.status = 200
+    res.json(pets[index]);
+  });
+});
+
 app.listen(port, () => {
-    console.log('working');
-})
+  console.log(`Brain Hurty`);
+});
